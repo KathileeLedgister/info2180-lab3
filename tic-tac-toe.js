e9d9086d983f708d2a534aa23a4b39c51de8aead
KathileeLedgister <k.alex.ledgister@gmail.com>
2021-10-09 02:28:56 -0500

    Exercise 2 Complete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,31 @@
+/*
+ * Didn't use an array because it could cause data inconsistency
+ */
 
+var lastSquare = "O";
+
+function onclickSquare(event){
+    let obj = this;
+    let classLst = obj.classList;
+    if(!(classLst.contains("X") || classLst.contains("O"))){
+        if(lastSquare === "O"){
+            classLst.add("X");
+            obj.textContent = "X";
+            lastSquare = "X";
+        }
+        else{
+            classLst.add("O");
+            obj.textContent = "O";
+            lastSquare = "O";
+        }
+    }
+}
 
 window.onload = function() {
-    let els= document.querySelectorAll("#board > div");
-    Object.values(els).forEach((val) =>  {
-        let classes = val.classList;
-        classes.add("square");
+    let elementList= document.querySelectorAll("#board > div");
+    Object.values(elementList).forEach((singleElement) =>  {
+        let classLst = singleElement.classList;
+        classLst.add("square");
+        singleElement.addEventListener("click", onclickSquare);
     });
-
-    let i = 1;
 };
