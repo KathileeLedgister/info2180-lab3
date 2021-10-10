1628e7fcaff93b2171bb9388c96cbbbd007b692e
KathileeLedgister <k.alex.ledgister@gmail.com>
2021-10-10 00:37:54 -0500

    Exercise 4  Complete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,22 +3,31 @@
  */
 
 var lastSquare = "O";
+var gameOver = false;
+var originalStatusMsg = "";
 
 function onclickSquare(event){
-    let obj = this;
-    let classLst = obj.classList;
-    if(!(classLst.contains("X") || classLst.contains("O"))){
-        if(lastSquare === "O"){
-            classLst.add("X");
-            obj.textContent = "X";
-            lastSquare = "X";
+    do{
+        if(gameOver)
+            break;
+
+        let obj = this;
+        let classLst = obj.classList;
+        if(!(classLst.contains("X") || classLst.contains("O"))){
+            lastSquare=(lastSquare === "O"? "X":"O");
+            classLst.add(lastSquare);
+            obj.textContent = lastSquare;
+
+            let nRow = obj.getAttribute("data-row");
+            if(document.querySelectorAll("#board > div[data-row='"+nRow+"']."+lastSquare).length === 3){
+                let statusBar = document.getElementById("status");
+                classLst = statusBar.classList;
+                classLst.add("status", "you-won");
+                statusBar.textContent = "Congratulations! " +lastSquare+" is the Winner!";
+                gameOver=true;
+            }
         }
-        else{
-            classLst.add("O");
-            obj.textContent = "O";
-            lastSquare = "O";
-        }
-    }
+    }while(false);
 }
 
 function onmouseEnter(event){
@@ -35,12 +44,15 @@ function onmouseOut(event){
 
 window.onload = function() {
     let elementList= document.querySelectorAll("#board > div");
-    Object.values(elementList).forEach((singleElement) =>  {
+    Object.values(elementList).forEach((singleElement, index) =>  {
         let classLst = singleElement.classList;
         classLst.add("square");
         singleElement.addEventListener("click", onclickSquare);
         singleElement.addEventListener("mouseenter", onmouseEnter);
         singleElement.addEventListener("mouseout", onmouseOut);
+        singleElement.setAttribute("data-row", Math.floor(index/3));
+        let statusBar = document.getElementById("status");
+        originalStatusMsg = statusBar.textContent;
     });
 };
 
